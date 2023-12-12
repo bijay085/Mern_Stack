5cfa1ea5af15aabccf57ffcf4e3f99c1c679a432
Bijay koirala <bijaykoirala162@gmail.com>
2023-12-12 06:34:57 +0545

    Initial commit

---
+++ JS/operators.js
@@ -0,0 +1,113 @@
+//operators -- day 5
+
+//defination : Operators are pre-defined sign and symbol that are designed to perform designated operations on operand 
+
+// Types of operators :
+/*
+----------------------------------------------------------------------------------------------------------
+//===========> 1. Arithemetic operators
+a. addition (+)
+b. subtraction (-)
+c. multiplication (*)
+d. division (/)
+e. modulo (%)
+f. exponential (**) 
+g. concat (+)
+----------------------------------------------------------------------------------------------------------
+*/
+
+/*
+//  a. addition (+)
+ let num1 = 4;
+ let num2 = 5;
+ console.log(num1 + num2);
+
+//  b. subtraction (-)
+console.log(num1 - num2);
+
+//  c. multiplication (*)
+console.log(num1 * num2);
+
+//  d. division (/)
+console.log(num1 / num2);
+
+//  e. modulo (%)
+console.log(num1 % num2);
+
+// f. exponential (**) 
+ console.log(num1 ** num2);
+
+//  g. concat (+)
+console.log("ME" + "Ghost");
+*/
+
+/*
+----------------------------------------------------------------------------------------------------------
+//===========>2. Comparison (Relational) operators
+a. greater than (>)
+b. less than (<)
+c. greater than equal to (>=)
+d. less than equal to (<=)
+e. loose equals to (==)  :: value mtra check grx data types does not matter
+f. strict equals to (===)  :: check value and data type too , both most match
+g. not equal to (!=)
+----------------------------------------------------------------------------------------------------------
+*/
+
+/*
+console.log (6 == 6) ;
+console.log (6 == "6");
+console.log(6===6)
+console.log (6 === "6");
+console.log (6 != 6);
+console.log (6!= "6");
+console.log (5 != 9);
+console.log (3!= "9");
+*/
+
+/*
+----------------------------------------------------------------------------------------------------------
+////===========> 3. Assignment operators : used to assign value to a variable 
+a. equals to (=)
+b. arthimetic assignment operator 
+    eg : variable += 4 :: variable = variable + 4
+    eg : a -= 6; :: a = a -6
+    eg : a*=7;
+    eg : a /= 4;
+    eg : a **= 5;
+    eg : a %= 1;
+----------------------------------------------------------------------------------------------------------
+*/
+
+/*
+----------------------------------------------------------------------------------------------------------
+// 4. Logical operators :: boolean expression haru lai combine grnu paryo vane 
+
+a. logical and (&&)  :: it combine two or more boolean values and expression and returns true if all of them is true and false if at least one of them are flase.
+f && f && f = f
+t && t && f = f
+t && t && t = t
+
+b. logical or (||)  :: it combine two or more boolean values and expression and returns true if at least one of them are true and returns false if all of them are false.
+f || f || f = f
+t || t || f = t
+t || t || t = t
+
+==> increament , deacrement , type of () all are unary opertor.
+
+c. logical not (!) :: It is a unary logical operator that inerts the boolean value or expressions.
+!t = f
+!f = t
+----------------------------------------------------------------------------------------------------------
+*/
+
+/*
+console.log(9>=10 && 5>=5 && true && 3>10); //false 
+console.log((9>=10 || 5>=5 || false || 3>10)); //true
+
+// In JS operators like ( and  or ) do traversing , so if they is ( true && true && true && true ) :: the output will be true :: and this true is from last equation. 
+*/
+
+
+// 5. Special operatos 
+
---
+++ JS/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+if (age >= 18){
+//     console.log("You can vote.");
+// }
+
+// else{
+//     //do nothing;
+// }
