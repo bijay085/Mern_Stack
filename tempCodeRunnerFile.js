cb7145281311ab0ecf0afd6b72e98e66a5f5bfc3
0xYujan <yujanr4@gmail.com>
2023-12-26 06:40:59 +0545

    functions

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+//example 2 
+// let whenYouWillBe16 = function(age){
+//     let output2 = (age >= 16) ? (age > 16) ? `You were 16 before ${age}-${16} year.` :`You are sweet 16`: `You will be after ${16}-${age} year.`
+//     return output2;
+// }
